9245c044cc1fae162b2c505110ed970e4c8f25b4
NisanurBulut <nisanurrunasin@gmail.com>
2017-03-22 16:01:21 +0200

    22.03.2017 16:01
    sepet afix responsive sorun çözümü

---
+++ FRCRM/js/sepet-js/sepet-js.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    $('#nav').affix(
+        {
+            offset: {
+                top: 100,
+                bottom: 50
+
+            }
+        }
+    );
+});
+
+
+var fixAffixWidth = function () {
+    $('[data-spy="affix"]').each(function () {
+        $(this).width($(this).parent().width());
+    });
+}
+fixAffixWidth();
+$(window).resize(fixAffixWidth);
