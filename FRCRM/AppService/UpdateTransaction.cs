3ba28eac4114ee3defe68d858e19eaafb4ea0126
abltcy <bilgi@ahmetbaltaci.com>
2017-04-25 14:15:56 +0300

    Rest Api Vs.

---
+++ FRCRM/AppService/UpdateTransaction.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Npgsql;
+using System.Configuration;
+using System.Web.Script.Serialization;
+using System.Text;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Dynamic;
+using System.Globalization;
+using System.Data;
+
+namespace FRCRM.AppService
+{
+    public class UpdateTransaction
+    {
+
+        string islem;
+        public string RunNpgsqlTransaction(string sql)
+        {
+            string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+            using (NpgsqlConnection pgConnection = new NpgsqlConnection(constr))
+            {
+                pgConnection.Open();
+                // Start a local transaction
+                NpgsqlTransaction myTrans = pgConnection.BeginTransaction(System.Data.IsolationLevel.ReadCommitted);
+                NpgsqlCommand pgCommand = pgConnection.CreateCommand();
+                try
+                {
+
+
+                    pgCommand.CommandText = sql;
+                    pgCommand.ExecuteNonQuery();
+                    myTrans.Commit();
+                    islem = "0";
+                    Console.WriteLine("işlem tamam");
+                }
+                catch (Exception e)
+                {
+                    myTrans.Rollback();
+                    Console.WriteLine(e.ToString());
+                    Console.WriteLine("Hata oluştu işlem geri alındı");
+                    islem = e.ToString();
+                }
+                finally
+                {
+                    pgCommand.Dispose();
+                    myTrans.Dispose();
+                }
+            }
+            return islem;
+        }
+    }
+}
---
+++ FRCRM/Controllers/CheckController.cs
@@ -0,0 +1,25 @@
+﻿using FRCRM.AppService;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Script.Services;
+
+namespace FRCRM.Controllers
+{
+    public class CheckController : ApiController
+    {
+        public DataTable Get(string account_id,string adsno,string state)
+        {
+            DataTable response = new DataTable("response");
+            UpdateTransaction updt = new UpdateTransaction();
+            string trans = updt.RunNpgsqlTransaction("update ads_order_state set order_state = "+state+" where adsno = "+adsno+" and account_id ="+account_id);
+            response.Columns.Add("response");
+            response.Rows.Add(trans);
+            return response;
+        }
+    }
+}
